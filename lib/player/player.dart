e78cd8858a7bd9833a110dfe130d6680862c076c
福富 道徳 <wa2noko>
2022-06-14 14:03:48 +0900

    途中経過2

--- lib/player/player.dart
+++ lib/player/player.dart
@@ -8,71 +8,83 @@ class Player extends PlayerBase {
   final BoardInfo boardInfo;
 
   @override
-  List<List<BlockStateKind>> updateUnder(int rowNumber, int columnNumber) {
-    super.updateUnder(rowNumber, columnNumber);
+  List<List<BlockStateKind>> updateUnder(int columnNumber, int rowNumber) {
+    super.updateUnder(columnNumber, rowNumber);
     var result = boardInfo.boardState;
-    result[rowNumber][columnNumber] = blockStateKind;
-
-    for (var i = columnNumber + 1; i <= 8; i++) {
-      if (boardInfo.boardState[rowNumber][i] == BlockStateKind.notExistStone || boardInfo.boardState[rowNumber][i] == blockStateKind) break;
-      if (result[rowNumber][i] == blockStateKind) {
-        for (var j = columnNumber + 1; j < i; j++) {
-          result[rowNumber][j] = blockStateKind;
-        }
-      }
-    }
-
-    return result;
-  }
-
-  @override
-  List<List<BlockStateKind>> updateTop(int rowNumber, int columnNumber) {
-    super.updateTop(rowNumber, columnNumber);
-    var result = boardInfo.boardState;
-    result[rowNumber][columnNumber] = blockStateKind;
-
-    for (var i = columnNumber - 1; i >= 1; i--) {
-      if (boardInfo.boardState[rowNumber][i] == BlockStateKind.notExistStone || boardInfo.boardState[rowNumber][i] == blockStateKind) break;
-      if (result[rowNumber][i] == blockStateKind) {
-        for (var j = columnNumber - 1; j > i; j--) {
-          result[rowNumber][j] = blockStateKind;
-        }
-      }
-    }
-    return result;
-  }
-
-  @override
-  List<List<BlockStateKind>> updateLeft(int rowNumber, int columnNumber) {
-    super.updateRight(rowNumber, columnNumber);
-    var result = boardInfo.boardState;
-    result[rowNumber][columnNumber] = blockStateKind;
-
-    for (var i = rowNumber - 1; i >= 1; i--) {
-      if (boardInfo.boardState[i][columnNumber] == BlockStateKind.notExistStone || boardInfo.boardState[i][columnNumber] == blockStateKind) break;
-      if (result[i][columnNumber] == blockStateKind) {
-        for (var j = rowNumber - 1; j > i; j--) {
-          result[j][columnNumber] = blockStateKind;
-        }
-      }
-    }
-    return result;
-  }
-
-  @override
-  List<List<BlockStateKind>> updateRight(int rowNumber, int columnNumber) {
-    super.updateRight(rowNumber, columnNumber);
-    var result = boardInfo.boardState;
-    result[rowNumber][columnNumber] = blockStateKind;
+    result[columnNumber][rowNumber] = blockStateKind;
+    print(rowNumber);
+    print(blockStateKind);
 
     for (var i = rowNumber + 1; i <= 8; i++) {
-      if (boardInfo.boardState[i][columnNumber] == BlockStateKind.notExistStone || boardInfo.boardState[i][columnNumber] == blockStateKind) break;
-      if (result[i][columnNumber] == blockStateKind) {
+      // 更新方向。真下が同色ならすぐに抜けて、更新方向に抜けがある場合も更新せずに抜ける。
+      if (boardInfo.boardState[columnNumber][i] == BlockStateKind.notExistStone || boardInfo.boardState[columnNumber][rowNumber + 1] == blockStateKind) break;
+      if (result[columnNumber][i] == blockStateKind) {
         for (var j = rowNumber + 1; j < i; j++) {
-          result[j][columnNumber] = blockStateKind;
+          result[columnNumber][j] = blockStateKind;
         }
       }
     }
+
+    return result;
+  }
+
+  @override
+  List<List<BlockStateKind>> updateTop(int columnNumber, int rowNumber) {
+    super.updateTop(columnNumber, rowNumber);
+    var result = boardInfo.boardState;
+    result[columnNumber][rowNumber] = blockStateKind;
+
+    for (var i = rowNumber - 1; i >= 1; i--) {
+      if (boardInfo.boardState[columnNumber][i] == BlockStateKind.notExistStone || boardInfo.boardState[columnNumber][rowNumber - 1] == blockStateKind) break;
+      if (result[columnNumber][i] == blockStateKind) {
+        for (var j = rowNumber - 1; j > i; j--) {
+          result[columnNumber][j] = blockStateKind;
+        }
+      }
+    }
+    return result;
+  }
+
+  @override
+  List<List<BlockStateKind>> updateLeft(int columnNumber, int rowNumber) {
+    super.updateRight(columnNumber, rowNumber);
+    var result = boardInfo.boardState;
+    result[columnNumber][rowNumber] = blockStateKind;
+
+    for (var i = columnNumber - 1; i >= 1; i--) {
+      if (boardInfo.boardState[i][rowNumber] == BlockStateKind.notExistStone || boardInfo.boardState[columnNumber - 1][rowNumber] == blockStateKind) break;
+      if (result[i][rowNumber] == blockStateKind) {
+        for (var j = columnNumber - 1; j > i; j--) {
+          result[j][rowNumber] = blockStateKind;
+        }
+      }
+    }
+    return result;
+  }
+
+  @override
+  List<List<BlockStateKind>> updateRight(int columnNumber, int rowNumber) {
+    super.updateRight(columnNumber, rowNumber);
+    var result = boardInfo.boardState;
+    result[columnNumber][rowNumber] = blockStateKind;
+
+    for (var i = columnNumber + 1; i <= 8; i++) {
+      if (boardInfo.boardState[i][rowNumber] == BlockStateKind.notExistStone || boardInfo.boardState[columnNumber + 1][rowNumber] == blockStateKind) break;
+      if (result[i][rowNumber] == blockStateKind) {
+        for (var j = columnNumber + 1; j < i; j++) {
+          result[j][rowNumber] = blockStateKind;
+        }
+      }
+    }
+    return result;
+  }
+
+  @override
+  List<List<BlockStateKind>> updateRightUpper(int columnNumber, int rowNumber) {
+    super.updateRightUpper(columnNumber, rowNumber);
+    var result = boardInfo.boardState;
+    result[columnNumber][rowNumber] = blockStateKind;
+
     return result;
   }
 }
--- lib/player/player_base.dart
+++ lib/player/player_base.dart
@@ -1,8 +1,10 @@
 import 'package:testoth/board/board_info.dart';
 
 abstract class PlayerBase {
-  List<List<BlockStateKind>>? updateUnder(int rowNumber, int columnNumber) {}
-  List<List<BlockStateKind>>? updateRight(int rowNumber, int columnNumber) {}
-  List<List<BlockStateKind>>? updateTop(int rowNumber, int columnNumber) {}
-  List<List<BlockStateKind>>? updateLeft(int rowNumber, int columnNumber) {}
+  List<List<BlockStateKind>>? updateUnder(int columnNumber, int rowNumber) {}
+  List<List<BlockStateKind>>? updateRight(int columnNumber, int rowNumber) {}
+  List<List<BlockStateKind>>? updateTop(int columnNumber, int rowNumber) {}
+  List<List<BlockStateKind>>? updateLeft(int columnNumber, int rowNumber) {}
+  List<List<BlockStateKind>>? updateRightUpper(int columnNumber, int rowNumber) {}
+  List<List<BlockStateKind>>? updateRightLower(int columnNumber, int rowNumber) {}
 }
